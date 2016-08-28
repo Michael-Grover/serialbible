70c0207c0eb8b647a64ff3e05b3382f8f448d28f
MichaelGroverDesign <mgrover@risd.edu>
2016-08-27 20:49:49 -0400

    initial commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    
+
+});
+
+
+/*
+    (function() {
+        var path = '//easy.myfonts.net/v2/js?sid=98(font-family=American+Text)&key=z8ivX3wOTf',
+            protocol = ('https:' == document.location.protocol ? 'https:' : 'http:'),
+            trial = document.createElement('script');
+        trial.type = 'text/javascript';
+        trial.async = true;
+        trial.src = protocol + path;
+        var head = document.getElementsByTagName("head")[0];
+        head.appendChild(trial);
+    })();
+*/
